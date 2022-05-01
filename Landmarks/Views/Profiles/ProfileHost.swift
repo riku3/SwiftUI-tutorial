d17b85b319b21ad9e03a64def70a1e26687f5df5
riku3 <takarakuten62@gmail.com>
2022-05-01 17:36:56 +0900

    start Working with UI Controls complete section 1

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by riku on 2022/05/01.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
