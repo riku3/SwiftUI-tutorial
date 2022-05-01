6fad7c8913b65f13241fd2b2f7eb4fb44849b891
riku3 <takarakuten62@gmail.com>
2022-05-01 18:51:16 +0900

    start Interfacing with UIKit complete section 1

---
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by riku on 2022/05/01.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+}
