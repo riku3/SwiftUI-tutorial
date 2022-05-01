d5a629d6f74a38bffd26daf6c322066f3630130b
riku3 <takarakuten62@gmail.com>
2022-05-01 12:48:12 +0900

    complete section 6

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -10,8 +10,14 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
         }
     }
 }
